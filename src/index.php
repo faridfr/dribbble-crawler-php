e6e53bf37e96aa540fae7769a942621dabcd946b
Farid Froozan <froozan@yahoo.com>
2022-04-26 16:53:45 +0430

    test crawler functions

--- src/index.php
+++ src/index.php
@@ -5,12 +5,22 @@ use Goutte\Client;
 $client = new Client();
 $data = [];
 $crawler = $client->request('GET', 'https://www.dribbble.com');
-$crawler->filter('.shot-thumbnail-link')->each(function ($node) use(&$data) {
+$crawler->filter('.shot-thumbnail-link')->each(function ($node) use(&$data,$client) {
     // print $node->text(). '-'. $node->attr('href')."\n";
 
     $item = new StdClass();
+    $item->colors = [];
     $item->href = $node->attr('href');
     $item->text = $node->text();
+
+    $new = $client->click($node->link());
+    $new->filter('head > title')->each(function ($node) use(&$data,$client,$item) {
+        $item->title = $node->text();
+    });
+    $new->filter('li.color > a')->each(function ($node) use(&$data,$client,$item) {
+        array_push($item->colors, $node->text());
+    });
+
     array_push($data,$item);
 });
 
